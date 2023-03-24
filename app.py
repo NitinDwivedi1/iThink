fab6cc3a4fb508b50854af110e07677c3e695101
nitin <nitindwivedi267@gmail.com>
2023-03-25 04:34:51 +0530

    updated database and other updations

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template,request,redirect,url_for, flash
+from flask import Flask, render_template,request,redirect,url_for, flash, session
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, validators, StringField, SelectField, TextAreaField, SubmitField, widgets
 from wtforms.validators import InputRequired, Email, Length, ValidationError, EqualTo
-from wtforms.fields.html5 import DateField
+from wtforms.fields import DateField
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.sql.expression import update
@@ -10,52 +10,22 @@ from sqlalchemy.sql import func
 import email_validator
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 import os
-import pytz
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.urandom(16)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL') or "postgres://jugdfikgztzyav:ff22685a6eb9b3195dafce4d68d364ec54b2798d9e24616af02221de91647b72@ec2-23-21-4-7.compute-1.amazonaws.com:5432/datk3n7r12n6rb"
-db=SQLAlchemy(app)
-login_manager=LoginManager()
-login_manager.init_app(app)
-login_manager.login_view='login'
 
-class User(UserMixin, db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    fullname=db.Column(db.String(20))
-    email=db.Column(db.String(50), unique=True)
-    dob=db.Column(db.Date)
-    profession=db.Column(db.String(20))
-    username=db.Column(db.String(20), unique=True)
-    password=db.Column(db.String(200))
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root@123'
+app.config['MYSQL_DB'] = 'ithink_db'
+mysql = MySQL(app)
 
-class PostData(UserMixin, db.Model):
-    p_id=db.Column(db.Integer, primary_key=True)
-    PostCategory=db.Column(db.String(15))
-    title=db.Column(db.String(20))
-    content=db.Column(db.Text)
-    timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
-    id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-class PostComment(UserMixin, db.Model):
-    c_id=db.Column(db.Integer, primary_key=True)
-    comment=db.Column(db.Text)
-    timestamp=db.Column(db.DateTime(timezone=True), default=func.now())
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    p_id = db.Column(db.Integer, db.ForeignKey('post_data.p_id',ondelete='SET NULL'), nullable=False)
-
-class Notification(UserMixin, db.Model):
-    n_id=db.Column(db.Integer, primary_key=True)
-    commentator=db.Column(db.Integer,nullable=False)
-    timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
-    p_id=db.Column(db.Integer, db.ForeignKey('post_data.p_id'),nullable=False)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 class RegisterForm(FlaskForm):
     fullname = StringField('Full name', validators=[InputRequired(), Length(min=3, max=20, message='Field must be between 3 and 20 characters long.')])
@@ -66,15 +36,18 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80, message='Field must be between 8 and 80 characters long.')])
     confirmPassword = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password', message='Passwords didn\'t match!')])
 
+
 class UpdateForm(FlaskForm):
     fullname = StringField('Full name', validators=[InputRequired(), Length(min=3, max=20, message='Field must be between 3 and 20 characters long.')])
     email = StringField('Email id', validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)])
     dob = DateField('Date of birth',format="%Y-%m-%d", validators=[InputRequired()])
     profession = StringField('Profession', validators=[InputRequired(), Length(min=2, max=20, message='Field must be between 2 and 20 characters long.')])
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=3, max=20)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 class PostForm(FlaskForm):
     categories=["Technology", "Sports","Entertainment" , "Knowledge", "Lifestyle", "Fashion", "Cooking", "Politics"]
@@ -82,127 +55,222 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired()])
     content = TextAreaField('Content', validators=[InputRequired()])
 
+
 class CommentForm(FlaskForm):
     comment = StringField("Comment", validators=[InputRequired()])
 
+
 @app.route('/', methods=['GET','POST'])
 @app.route('/<pid>', methods=['GET','POST'])
-@login_required
-def index(pid=None):
-    result=db.session.execute('select p."PostCategory", p."title", p."content", p."timestamp", u."username", p."p_id" from "user" u, "post_data" p where u."id"=p."id" order by "timestamp" desc')
-    posts=[list(row) for row in result]
-    form=None
-    cmnts=None
+@app.route('/hide_cmnts/<cpid>', methods=['GET','POST'])
+def index(pid=None,cpid=None):
+    if 'logged in' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select p.post_category, p.title, p.content, p.timestamp, u.username, p.p_id from accounts u, post_data p where u.id=p.id order by timestamp desc')
+        result = cursor.fetchall()
+        print(result)
+        posts=[row for row in result]
+        form=None
+        cmnts=None
 
-    if pid != None:
-        ucmnts=db.session.execute('select c."comment", c."timestamp", u."username", p."p_id" from "post_comment" c, "user" u, "post_data" p where c."p_id"=p."p_id" and u."id"=c."id" ')
-        cmnts=[list(row) for row in ucmnts]
-        form =CommentForm()
-        if form.validate_on_submit():
-            postComment=PostComment(comment=form.comment.data,timestamp=func.now(),id=current_user.id,p_id=pid)
-            db.session.add(postComment)
-            db.session.commit()
-            notification=Notification(commentator=current_user.id,timestamp=func.now(),p_id=pid)
-            db.session.add(notification)
-            db.session.commit()
-            flash("Comment posted!",category='success')
-            return redirect(url_for('index'))
-    return render_template("index.html",posts=posts, form=form, cmnts=cmnts,page='index',pid=pid)
+        if pid != None:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('select c.comment, c.timestamp, u.username, p.p_id from post_comment c, accounts u, post_data p where c.p_id=p.p_id and u.id=c.id ')
+            ucmnts=cursor.fetchall()
+            print(ucmnts)
+            cmnts=[row for row in ucmnts]
+            form =CommentForm()
+            if form.validate_on_submit():
+                time=datetime.now()
+                comment = form.comment.data
+                timestamp = time
+                id = session['id']
+                p_id = pid
+                cursor.execute('INSERT INTO ithink_db.post_comment VALUES (NULL, %s, %s, %s, %s)',(comment, timestamp, id, p_id))
+                mysql.connection.commit()
+
+                commentator = session['id']
+                timestamp = time
+                p_id = pid
+                cursor.execute('INSERT INTO ithink_db.notification VALUES (NULL, %s, %s, %s)',
+                               (commentator, timestamp, p_id,))
+                mysql.connection.commit()
+                flash("Comment posted!",category='success')
+                return redirect(url_for('index'))
+        return render_template("index.html",posts=posts, form=form, cmnts=cmnts,page='index',pid=pid)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    msg=''
     form = RegisterForm()
-    if form.validate_on_submit():
+    # if form.validate_on_submit():
+    if request.method=='POST':
         hashed_password = generate_password_hash(form.password.data)
-        new_user=User(fullname=form.fullname.data,
-                      email=form.email.data,
-                      dob=form.dob.data,
-                      profession=form.profession.data,
-                      username=form.username.data,
-                      password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash("You have been registered successfully!", category='success')
-        return redirect(url_for("index"))
+        fullname = form.fullname.data,
+        email = form.email.data,
+        dob = form.dob.data,
+        profession = form.profession.data,
+        username = form.username.data,
+        password = hashed_password
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', str(email)):
+            msg = 'Invalid email address !'
+        # elif not re.match(r'[A-Za-z0-9]+', str(fullname)):
+        #     msg = 'name must contain only characters and numbers !'
+        else:
+            print("else")
+            cursor.execute('INSERT INTO ithink_db.accounts VALUES (NULL, % s, % s, % s, % s, % s, % s)',
+                           (username, password, email, fullname, dob, profession,))
+            mysql.connection.commit()
+            print("hello")
+            msg = 'You have successfully registered !'
+            flash("You have been registered successfully!", category='success')
+            print(msg)
+            print("hiiiii")
+            return redirect(url_for("login"))
+        print(msg)
+        flash(msg, category='error')
     return render_template('register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    msg=''
     form = LoginForm()
     if form.validate_on_submit():
-        user=User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for("index"))
-        flash("Invalid username or password!", category='error')
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
+        account = cursor.fetchone()
+        print(account)
+        if account:
+            if check_password_hash(account['password'], password):
+                session['logged in'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                msg = 'Logged in successfully !'
+                flash(msg, category='success')
+                return redirect(url_for('index'))
+            else:
+                print("wrong password")
+                msg = 'Incorrect Password'
+        else:
+            print("wrong username")
+            msg="Account doesn't exist"
+        flash(msg, category='error')
         return redirect(url_for('login'))
 
     return render_template("login.html", form=form)
+
+
 @app.route('/logout')
-@login_required
 def logout():
-    logout_user()
-    return redirect(url_for('login'))
+    if 'logged in' in session:
+        session.pop('logged in', None)
+        session.pop('id', None)
+        session.pop('username', None)
+        # Redirect to login page
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/profile')
-@login_required
 def profile():
-    fullname = current_user.fullname
-    email = current_user.email
-    dob = current_user.dob
-    profession = current_user.profession
-    username = current_user.username
+    if 'logged in' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select fullname, email, dob, profession, username from accounts where id=%s', [session['id']])
+        user = cursor.fetchone()
+        print(user)
+        fullname = user['fullname']
+        email = user['email']
+        dob = user['dob']
+        profession = user['profession']
+        username = user['username']
 
-    result = db.session.execute('select "PostCategory", "title", "content", "p_id" ,"timestamp" from "post_data"  where "id"=:val order by "timestamp" desc', {'val':current_user.id})
-    posts = [list(row) for row in result]
-    return render_template('profile.html',fullname=fullname,
-                                          email=email,
-                                          dob=dob,
-                                          profession=profession,
-                                          username=username,
-                                          posts=posts,page='profile')
+        cursor.execute('select post_category, title, content, p_id ,timestamp from post_data  where id=%s order by timestamp desc', [session['id']])
+        result = cursor.fetchall()
+        posts = [row for row in result]
+        return render_template('profile.html',fullname=fullname,
+                                              email=email,
+                                              dob=dob,
+                                              profession=profession,
+                                              username=username,
+                                              posts=posts,page='profile')
+    else:
+        return redirect(url_for('login'))
+
+
 @app.route('/post', methods=['GET','POST'])
-@login_required
 def post():
-    form = PostForm()
-    if form.validate_on_submit():
-        postData=PostData(PostCategory=form.PostCategory.data,
-                          title=form.title.data,
-                          content=form.content.data,
-                          id=current_user.id,
-                          timestamp=func.now())
-        db.session.add(postData)
-        db.session.commit()
-        flash('Posted!', category='success')
-        return redirect(url_for('index'))
-    return render_template('post.html',form=form,page='post')
+    if 'logged in' in session:
+        form = PostForm()
+        if form.validate_on_submit():
+            PostCategory = form.PostCategory.data
+            title = form.title.data
+            content = form.content.data
+            id = session['id']
+            timestamp = datetime.now()
+
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('INSERT INTO ithink_db.post_data VALUES(NULL, %s, %s, %s, %s, %s)', (PostCategory, title, content, timestamp, id))
+            mysql.connection.commit()
+            flash('Posted!', category='success')
+            return redirect(url_for('index'))
+        return render_template('post.html',form=form,page='post')
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/edit',methods=['GET','POST'])
 def editInfo():
-    form=UpdateForm()
-    if form.validate_on_submit():
-        db.session.execute('update "user" set "fullname"=:val1, "email"=:val2, "dob"=:val3, "profession"=:val4 where "id"=:val5',{'val1':form.fullname.data,'val2':form.email.data,'val3':form.dob.data,'val4':form.profession.data,'val5':current_user.id})
-        db.session.commit()
-        flash("Profile updated!", category='success')
-        return redirect(url_for('profile'))
-    return render_template('update.html',form=form)
+    if 'logged in' in session:
+        form=UpdateForm()
+        if form.validate_on_submit():
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('update ithink_db.accounts set fullname=%s, email=%s, dob=%s, profession=%s where id=%s', (form.fullname.data, form.email.data, form.dob.data, form.profession.data, session['id']))
+            mysql.connection.commit()
+            flash("Profile updated!", category='success')
+            return redirect(url_for('profile'))
+        return render_template('update.html',form=form)
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/delete/<did>',methods=['GET','POST'])
-@login_required
 def deletePost(did):
-    db.session.execute('delete from post_data where "p_id"=:val',{'val':did})
-    db.session.commit()
-    flash("Post deleted!", category='success')
-    return redirect(url_for('profile'))
+    if 'logged in' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('delete from ithink_db.post_data where p_id=%s',(did))
+        mysql.connection.commit()
+        flash("Post deleted!", category='success')
+        return redirect(url_for('profile'))
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/notification', methods=['GET','POST'])
-@login_required
 def notification():
-    notifications=db.session.execute('select u."username", n."p_id", n."timestamp" from "user" u, "notification" n where u."id"=n."commentator" order by "timestamp" desc')
-    notifications = [list(row) for row in notifications]
-    userPosts = db.session.execute('select "p_id" from post_data where post_data."id"=:val',{'val':current_user.id})
-    userPosts = [list(row) for row in userPosts]
-    return render_template('notification.html',notifications=notifications, userPosts=userPosts,page='notification')
+    if 'logged in' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select u.username, n.p_id, n.timestamp from accounts u, notification n where u.id=n.commentator order by timestamp desc')
+        notifications = cursor.fetchall()
+        print(notifications)
+        notifications = [row for row in notifications]
+        cursor.execute('select p_id from post_data where post_data.id=%s', [session['id']])
+        userPosts = cursor.fetchall()
+        print(userPosts)
+        userPosts = [row for row in userPosts]
+        return render_template('notification.html',notifications=notifications, userPosts=userPosts,page='notification')
+    else:
+        return redirect(url_for('login'))
